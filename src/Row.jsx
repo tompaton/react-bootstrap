1875b1eb743eaed24ad9fb40bbadf77bfee48705
Tom Paton <tom.paton@gmail.com>
2014-10-06 21:57:49 +1100

    add display prop to Row component

--- src/Row.jsx
+++ src/Row.jsx
@@ -3,20 +3,25 @@ var joinClasses = require('./utils/joinClasses');
 
 var Row = React.createClass({
   propTypes: {
-    componentClass: React.PropTypes.node.isRequired
+    componentClass: React.PropTypes.node.isRequired,
+    display: React.PropTypes.bool
   },
 
   getDefaultProps: function () {
     return {
-      componentClass: 'div'
+      componentClass: 'div',
+      display: true
     };
   },
 
   render: function () {
     var ComponentClass = this.props.componentClass;
 
+    // hide if display is false, show if undefined or true
     return (
-      <ComponentClass {...this.props} className={joinClasses(this.props.className, 'row')}>
+      <ComponentClass {...this.props}
+                      className={joinClasses(this.props.className, 'row')}
+                      style={{display: this.props.display==false ? 'none' : 'block'}}>
         {this.props.children}
       </ComponentClass>
     );
