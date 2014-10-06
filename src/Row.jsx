5e1b2efa946bb53221d7a77278b5c0d94a9e858c
Tom Paton <tom.paton@gmail.com>
2014-10-06 21:57:49 +1100

    add display prop to Row component

--- src/Row.jsx
+++ src/Row.jsx
@@ -6,20 +6,24 @@ var CustomPropTypes = require('./utils/CustomPropTypes');
 
 var Row = React.createClass({
   propTypes: {
-    componentClass: CustomPropTypes.componentClass.isRequired
+    componentClass: CustomPropTypes.componentClass.isRequired,
+    display: React.PropTypes.bool
   },
 
   getDefaultProps: function () {
     return {
-      componentClass: React.DOM.div
+      componentClass: React.DOM.div,
+      display: true
     };
   },
 
   render: function () {
     var componentClass = this.props.componentClass;
 
+    // hide if display is false, show if undefined or true
     return this.transferPropsTo(
-      <componentClass className="row">
+      <componentClass className="row"
+                      style={{display: this.props.display==false ? 'none' : 'block'}}>
         {this.props.children}
       </componentClass>
     );
